e8a7f890cccdd33eef7e822203773dc487a7d759
Manuel Korfmann <manu@korfmann.info>
2011-06-06 00:16:44 +0200

    indentation

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -1,28 +1,28 @@
 require 'helper'
 describe 'Parser' do
 
-	describe 'from_string' do
-		before do
-			@csv = 'foo;bar;foz;b4z;"foo;bar"/2;4;5;no;o/lala;bla;woot;fuck;o'
-		end
-		describe 'costum seperators' do
-			it "extracts the header" do
-				assert_equal %w{foo bar foz b4z foo;bar}, FightCSV::Parser.from_string(@csv, seperator: ';', dataset_seperator:'/').first[:data_source].header
-			end
+  describe 'from_string' do
+    before do
+      @csv = 'foo;bar;foz;b4z;"foo;bar"/2;4;5;no;o/lala;bla;woot;fuck;o'
+    end
+    describe 'costum seperators' do
+      it "extracts the header" do
+        assert_equal %w{foo bar foz b4z foo;bar}, FightCSV::Parser.from_string(@csv, seperator: ';', dataset_seperator:'/').first[:data_source].header
+      end
 
-			it 'returns an array with a hash including the body of the csv + metadata' do
-				parsed = FightCSV::Parser.from_string(@csv, seperator: ';', dataset_seperator: '/')
-				assert_equal nil, parsed.first[:data_source].filename
-				assert_equal %w{2 4 5 no o}, parsed.first[:body].first
-			end
-		end
-	end
+      it 'returns an array with a hash including the body of the csv + metadata' do
+        parsed = FightCSV::Parser.from_string(@csv, seperator: ';', dataset_seperator: '/')
+        assert_equal nil, parsed.first[:data_source].filename
+        assert_equal %w{2 4 5 no o}, parsed.first[:body].first
+      end
+    end
+  end
 
-	describe 'from_files' do
-		it "contains metadata about the filename" do
-			data = FightCSV::Parser.from_files([fixture('recipes.csv'), fixture('cocktails.csv')])
-			assert_match 'recipes.csv', data.first[:data_source].filename
-			assert_match 'cocktails.csv', data.last[:data_source].filename
-		end
-	end
+  describe 'from_files' do
+    it "contains metadata about the filename" do
+      data = FightCSV::Parser.from_files([fixture('recipes.csv'), fixture('cocktails.csv')])
+      assert_match 'recipes.csv', data.first[:data_source].filename
+      assert_match 'cocktails.csv', data.last[:data_source].filename
+    end
+  end
 end
--- test/test_schema.rb
+++ test/test_schema.rb
@@ -1,20 +1,20 @@
 require 'helper'
 describe 'Schema' do
-	describe 'DSL' do
-		it 'allows to easily create a field' do
-			schema = FightCSV::Schema.new
-			converter = ->(value) { value.upcase }
+  describe 'DSL' do
+    it 'allows to easily create a field' do
+      schema = FightCSV::Schema.new
+      converter = ->(value) { value.upcase }
 
-			schema.field "Name", {
-				required: true,
-				converter: converter,
-				validator: /\w*/
-			}
+      schema.field "Name", {
+        required: true,
+        converter: converter,
+        validator: /\w*/
+      }
 
-			field = schema.fields.first
-			assert_equal true, field.required
-			assert_equal converter, field.converter
-			assert_equal /\w*/, field.validator
-		end
-	end
+      field = schema.fields.first
+      assert_equal true, field.required
+      assert_equal converter, field.converter
+      assert_equal /\w*/, field.validator
+    end
+  end
 end
