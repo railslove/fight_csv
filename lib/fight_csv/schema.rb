4ff779f9e63e4e0c0e948dadb5b8dcdbf421ea05
Manuel Korfmann <manu@korfmann.info>
2011-08-18 13:55:31 +0200

    indentation

--- lib/fight_csv/schema.rb
+++ lib/fight_csv/schema.rb
@@ -1,17 +1,17 @@
 module FightCSV
-	class Schema
-		attr_accessor :fields
-		def initialize(filename = nil, &block)
-			self.fields = Array.new
+  class Schema
+    attr_accessor :fields
+    def initialize(filename = nil, &block)
+      self.fields = Array.new
       if String === filename
         self.instance_eval { eval(File.read(filename)) }
       elsif block
         self.instance_eval &block
       end
-		end
+    end
 
-		def field(fieldname, constructor_hash)
-			self.fields << Field.new(fieldname, constructor_hash)
-		end
-	end
+    def field(fieldname, constructor_hash)
+      self.fields << Field.new(fieldname, constructor_hash)
+    end
+  end
 end
